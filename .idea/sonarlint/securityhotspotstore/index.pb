f32c1aa68a1b2e26d1b276ad229708e939fb3b9d
SemihAslan123 <semihaslan2210@gmail.com>
2025-12-02 13:40:17 +0100

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+F
+src/VideoGrabDemo.java,c/9/c9f3d27d4217b36609c15de7e73781f0792b92ee
+F
+src/VideoGrabDemo.fxml,a/e/ae0a40bbefd20982bdbfc8880b08d874dfc86c42
+P
+ src/VideoGrabDemoController.java,6/3/63f4f5c98fe7d296d17e9bb20e9f7eee426ebc39
---
+++ src/VideoGrabDemo.java
@@ -0,0 +1,50 @@
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+
+
+import javafx.stage.WindowEvent;
+import org.opencv.core.Core;
+
+
+public class VideoGrabDemo extends Application {
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //VideoCapture capture = new VideoCapture(0); // 0 indique la première webcam
+        //Mat frame = new Mat();
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("VideoGrabDemo.fxml"));
+            BorderPane rootElement = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootElement, 1000, 600);
+
+            primaryStage.setTitle("Capture vidéo de la webcam avec OpenCV");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            VideoGrabDemoController controller = loader.getController();
+            primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
+                public void handle(WindowEvent we) {
+                    controller.setClosed();
+                }
+            }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
